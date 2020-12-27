37f6a816d751d635049e4bd503170d2a7a3222d5
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-12-28 01:10:35 +0900

    [add] init codes.

--- {{cookiecutter.project_slug}}/doc.go
+++ {{cookiecutter.project_slug}}/doc.go
@@ -1,1 +1,6 @@
+// Package {{ cookiecutter.project_slug }} provides a system by which it is possible to mock your objects
+// and verify calls are happening as expected.
+//
+// Example Usage
+//
 package {{ cookiecutter.project_slug }}
